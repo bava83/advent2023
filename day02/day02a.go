c806d29dce890461ace0c82244f20f72e4bf223d
bava <bavarialiu@gmail.com>
2024-01-02 14:57:25 +0800

    day02a complete

--- day02/day02a.go
+++ day02/day02a.go
@@ -9,7 +9,7 @@ import (
 	"strings"
 )
 
-func day02a(){
+func day02(){
   filePath := "./input.txt"
   readFile, err := os.Open(filePath)
   if err != nil {
@@ -24,18 +24,45 @@ func day02a(){
     s := fileScanner.Text()
     g := gameSet{valid: false}
     g.parseGameInput(s)
+    g.setValid()
     games = append(games, g)
   }
 
+  day02aSum := sumValidID(games) 
+  fmt.Printf("Sum of the valid ids: %d",day02aSum)
+  
+  day02bSum := 0
+
 }
+
+//find the sum of all valid games id
+func sumValidID(games []gameSet) int{
+  result := 0
+  for _,g := range games{
+    if g.valid{
+      result += g.id
+    }
+  }
+  return result
+}
+
+
+func sumMinPower(game []gameSet) int{
+  result := 0
+  for _,g := range games{
+
+  }
+} 
+
+
 
 type gameSet struct {
   id int
-  drawList []draw
+  drawList []Draw
   valid bool
 }
 
-type draw struct{
+type Draw struct{
   red int
   green int
   blue int
@@ -53,7 +80,7 @@ func (g *gameSet) parseGameInput(s string) {
   //split the 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green into draw
   gameInputString := strings.Split(inputArray[1], ";") 
   for _, drawString := range gameInputString{
-    d := draw{}
+    d := Draw{}
     //split the 3 blue, 4 red 
     cubeString := strings.Split(drawString, "," )
     for _, cube := range cubeString{
@@ -63,14 +90,14 @@ func (g *gameSet) parseGameInput(s string) {
       if err != nil {
         panic(err)
       }
-      //parse the 3, blue
+      //parse the 3 blue
       d.parseCubeData(n, cubeInput[1])
     }
     g.drawList = append(g.drawList, d)
   }
 }
 
-func (d *draw) parseCubeData(n int, color string){
+func (d *Draw) parseCubeData(n int, color string){
   switch color{
   case "red":
     d.red = n
@@ -78,5 +105,27 @@ func (d *draw) parseCubeData(n int, color string){
     d.green = n
   case "blue":
     d.blue = n 
+  }
+}
+
+func (g *gameSet) setValid(){
+  const VALIDRED= 12
+  const VALIDGREEN = 13
+  const VALIDBLUE = 14
+
+  for _, draws := range g.drawList  {
+    if draws.red > VALIDRED || draws.blue > VALIDBLUE || draws.green > VALIDGREEN{
+      g.valid = false
+      return 
+    } 
+  }
+  g.valid = true
+}
+
+func (g *gameSet) setPower(){
+  //copy one set of the values to minimum first
+  minDraw := g.drawList[0]
+  for _, draws := range g.drawList{
+    if draws.red 
   }
 }
